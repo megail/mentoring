0fb71c9f1cde7780ed3b28f629707f6e3e409627
mchen len son <Mikhail.ChenLenSon@homecredit.ru>
2013-10-30 12:02:28 +0400

    added javadocs

--- project-version-plugin/src/main/java/com/epam/mentoring/maven/HelloWorldPlugin.java
+++ project-version-plugin/src/main/java/com/epam/mentoring/maven/HelloWorldPlugin.java
@@ -6,6 +6,8 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 
 /**
+ * Simple hello-world plugin just to show how it works.
+ *
  * @author Mikhail_Chen-Len-Son@epam.com
  */
 @Mojo(name = "hello-world", defaultPhase = LifecyclePhase.COMPILE)
